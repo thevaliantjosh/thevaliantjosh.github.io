8ea0bce8f6dcb3bc290369a5f2272800b9f02959
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2023-03-15 12:18:26 -0500

    Added another professional experience

--- js/joshua-sprouse.js
+++ js/joshua-sprouse.js
@@ -9,12 +9,12 @@ $(function(){
     *  */
     $('#sidebar-icon').on("click", function(){
         // alert("You clicked me!");//this works
-        $(".sidebar-row-icon").toggleClass("sidebar-row-icon-shrink", 1000, "linear");
+        $(".sidebar-row-icon").toggleClass("sidebar-row-icon-shrink", 500, "linear");
         $(".sidebar-column-icon").toggleClass("sidebar-column-icon-shrink");
         $('#circle3, #circle6').toggle("drop", {direction: "left"}, "fast");
         $('#circle7, #circle8').toggle("drop", {direction: "up" }, "fast");
         $('#circle9').toggle("drop", {direction: "left"}, "fast")
-        $('.sidebar').toggleClass("sidebar-shrink", 1000, "linear");
+        $('.sidebar').toggleClass("sidebar-shrink", 500, "linear");
         $('.sidebar-container').toggle("fade");
 
     })
